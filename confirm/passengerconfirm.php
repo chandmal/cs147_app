b2844b8e5496eb4c1b6f4b8c540ad2e3f1d05cb2
Malaika Rayne Chandler <chandmal@corn06.stanford.edu>
2012-10-25 02:34:59 -0700

    Added popups to sample requests in request menu. Had two different kinds of requests to distinguish seen from not seen. Linked those popups to the confirmation pages for both drivers and passengers.

---
+++ confirm/passengerconfirm.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>
+		Confirmation
+        </title>
+        <link rel="stylesheet" href="https://ajax.aspnetcdn.com/ajax/jquery.mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
+        <link rel="stylesheet" href="my.css" />
+        <style>
+            /* App custom styles */
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
+        </script>
+        <script src="https://ajax.aspnetcdn.com/ajax/jquery.mobile/1.1.1/jquery.mobile-1.1.1.min.js">
+        </script>
+        <script src="my.js">
+        </script>
+    </head>
+    <body>
+        <!-- Home -->
+        <div data-role="page" id="page1">
+            <div data-theme="a" data-role="header">
+                <a data-role="button" href="../main/app.php" class="ui-btn-right">
+                    Menu
+                </a>
+                <h3>
+                    Confirmation
+                </h3>
+                <a data-role="button" data-rel="back" href="#page1" class="ui-btn-left">
+                    Back
+                </a>
+            </div>
+            <div data-role="content">
+                <h2>
+                    Thanks for submitting your request.
+                </h2>
+		  <h4>
+		      Now you're all set with a driver. They'll pick you up at the time specified.
+		  </h4>
+                <h4>
+                    What do you want to do next?
+                </h4>
+                <a data-role="button" data-theme="b" href="">
+                    Send more requests
+                </a>
+                <a data-role="button" data-theme="b" href="../requests/new.php">
+                    Go request menu
+                </a>
+                <a data-role="button" data-theme="b" href="../main/app.php">
+                    Go to main menu
+                </a>
+            </div>
+        </div>
+        <script>
+            //App custom javascript
+        </script>
+    </body>
+</html>
--- requests/new.php
+++ requests/new.php
@@ -4,16 +4,18 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>
+		Request Menu
         </title>
-        <link rel="stylesheet" href="https://ajax.aspnetcdn.com/ajax/jquery.mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
+        <!-- link rel="stylesheet" href="https://ajax.aspnetcdn.com/ajax/jquery.mobile/1.1.1/jquery.mobile-1.1.1.min.css" / -->
         <link rel="stylesheet" href="my.css" />
+	<link rel="stylesheet" href="http://jquerymobile.com/demos/1.2.0-alpha.1/css/themes/default/jquery.mobile-1.2.0-alpha.1.css" />
         <style>
             /* App custom styles */
         </style>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
         </script>
-        <script src="https://ajax.aspnetcdn.com/ajax/jquery.mobile/1.1.1/jquery.mobile-1.1.1.min.js">
-        </script>
+        <!-- script src="https://ajax.aspnetcdn.com/ajax/jquery.mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script -->
+	<script src="http://jquerymobile.com/demos/1.2.0-alpha.1/js/jquery.mobile-1.2.0-alpha.1.js"></script>
         <script src="my.js">
         </script>
     </head>
@@ -51,15 +53,44 @@
                         </li>
                     </ul>
                 </div>
-            </div>
-		<a href="" data-role="button" data-theme="e" data-icon="star" data-iconpos="right">
+            
+		<a href="#popuptest2" data-role="button" data-theme="e" data-icon="star" data-iconpos="right" class="ui-content" data-rel="popup" data-position-to="window">
 			[Name] wants a ride! (new request, theme e)
 		</a>
-		<a href="" data-role="button" data-theme="b">
+		<a href="#popuptest" class="ui-content"  data-role="button" data-theme="b" data-rel="popup" data-position-to="window">
 			[Name] wants to share! (one that's been looked at)
 		</a>
+		<div data-role="popup" id="popuptest" href="" data-overlay-theme="a">
+    			<div data-theme="d">
+       			 <h2>
+       			     [Name] [Last name]
+       			 </h2>
+				<h4> Time: From [Start] to [End] </h4>
+				<h4> Cost: $[Cost] </h4>
+				<h4> Rating: [Rating] </h4>
+				<a href="#../confirm/passengerconfirm.php" data-role="button" data-inline="true" data-theme="b">Accept and Pay</a>
+				<a href="" data-role="button" data-inline="true">Cancel</a>
+    			</div>
+		</div>
+		<div data-role="popup" id="popuptest2" href="#../popup/moreinforequest.php" data-overlay-theme="a">
+    			<div data-theme="d">
+       			 <h2>
+       			     [Name] [Last name]
+       			 </h2>
+				<h4> Time: From [Start] to [End] </h4>
+				<h4> Willing to pay: $[Price] </h4>
+				<h4> Rating: [Rating] </h4>
+				<a href="#../confirm/driverconfirm.php" data-role="button" data-inline="true" data-theme="b">Accept and Share</a>
+				<a href="" data-role="button" data-inline="true">Cancel</a>
+    			</div>
+		</div>	
+	
+	   </div>
 
         </div>
+
+	
+
         <script>
             //App custom javascript
         </script>
